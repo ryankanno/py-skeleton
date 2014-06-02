2fea3e51a6e93cd57102da8ae661982f02f9daad
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-06-02 07:19:09 -0400

    Added cleaned up setup

--- setup.py
+++ setup.py
@@ -1,18 +1,57 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import re
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
 packages = []
-requires = []
 
-with open('README.rst') as f:
+here = os.path.dirname(os.path.realpath(__file__))
+
+# Metadata
+
+meta = {}
+re_meta = re.compile(r'__(\w+?)__\s*=\s*(.*)')
+re_version = re.compile(r'VERSION\s*=.*?\((.*?)\)')
+strip_quotes = lambda s: s.strip("\"'")
+
+
+def add_version(match):
+    return {'VERSION': match.group(1).replace(" ", "").replace(",", ".")}
+
+
+def add_meta(match):
+    attr_name, attr_value = m.groups()
+    return {attr_name: strip_quotes(attr_value)}
+
+
+patterns = {
+    re_meta: add_meta,
+    re_version: add_version
+}
+
+
+with open(os.path.join(here, 'FILE_GOES_HERE'), 'r') as f:
+    for line in f:
+        for pattern, handler in patterns.items():
+            m = pattern.match(line.strip())
+            if m:
+                meta.update(handler(m))
+
+# Requires
+
+requires = []
+tests_require = ['flake8', 'mock', 'nose', 'nosexcover']
+
+with open(os.path.join(here, 'README.rst')) as f:
     readme = f.read()
 
-with open('CHANGES') as f:
+with open(os.path.join(here, 'CHANGES')) as f:
     changes = f.read()
 
 classifiers = [
@@ -20,18 +59,18 @@ classifiers = [
 
 setup(
     name='',
-    version='',
+    version=meta['VERSION'],
     description='',
     long_description=readme + '\n\n' + changes,
-    author='',
-    author_email='',
+    author=meta['author'],
+    author_email=meta['email'],
     url="",
     packages=packages,
     package_data={'': ['LICENSE']},
     package_dir={'': ''},
-    install_requires=[],
-    license='MIT',
-    tests_require=[],
+    install_requires=requires,
+    license=meta['license'],
+    tests_require=tests_require,
     classifiers=classifiers,
 )
 
