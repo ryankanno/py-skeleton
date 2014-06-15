b02f08111f2a30bf95a0093dd69be815412e5ad7
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-06-15 09:14:46 -0400

    Added version

--- py_skeleton/__init__.py
+++ py_skeleton/__init__.py
@@ -6,16 +6,14 @@ from collections import namedtuple
 version_info = namedtuple('version_info', ('major', 'minor', 'patch'))
 
 
-VERSION = version_info(0, 0, 1)
+VERSION = version_info(0, 0, 2)
 
 
 __title__ = 'py_skeleton'
 __version__ = '{0.major}.{0.minor}.{0.patch}'.format(VERSION)
-
 __author__ = 'Ryan Kanno'
-__copyright__ = 'Copyright 2014 Ryan Kanno'
-__license__ = 'MIT'
 __email__ = 'ryankanno@localkinegrinds.com'
-__status__ = 'Development'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014 Ryan Kanno'
 
 # vim: filetype=python
