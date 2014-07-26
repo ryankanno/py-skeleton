c07547082dd15121d99ab47383d4b1f73a23bd84
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-07-26 02:14:07 -0400

    Moved runner to py-skeleton

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ packages = []
 here = os.path.dirname(os.path.realpath(__file__))
 
 # Metadata
-
 meta = {}
 re_meta = re.compile(r'__(\w+?)__\s*=\s*(.*)')
 re_version = re.compile(r'VERSION\s*=.*?\((.*?)\)')
@@ -55,6 +54,11 @@ with open(os.path.join(here, 'CHANGES')) as f:
     changes = f.read()
 
 classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python',
+    'License :: OSI Approved :: MIT License',
+    'Topic :: Utilities'
 ]
 
 setup(
@@ -68,10 +72,12 @@ setup(
     packages=packages,
     package_data={'': ['LICENSE']},
     package_dir={'py_skeleton': 'py_skeleton'},
+    scripts=['bin/py-skeleton.py'],
     install_requires=requires,
     license=meta['license'],
     tests_require=tests_require,
     classifiers=classifiers,
+    setup_requires=['nose >= 1.3']
 )
 
 # vim: filetype=python
